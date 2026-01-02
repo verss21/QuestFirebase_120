d278bd2c0bf120e1b80bea61daf31f073c84f96e
verss21 <arjunafrizal01@gmail.com>
2026-01-02 22:04:03 +0700

    menambahkan container app

--- app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
@@ -1,4 +1,22 @@
 package com.example.firebase.repositori
 
-class ContainerApp {
+import android.app.Application
+
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
 }
