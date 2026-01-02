f73d85378a66bb049c9c3d0c389a1fc6fb4099c6
verss21 <arjunafrizal01@gmail.com>
2026-01-02 21:58:31 +0700

    menambahkan destinasi edit

---
+++ app/src/main/java/com/example/firebase/view/route/DestinasiEdit.kt
@@ -0,0 +1,10 @@
+package com.example.firebase.view.route
+
+import com.example.firebase.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
