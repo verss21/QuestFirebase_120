7bc33c8a35cf71068467c7c2f350bd71ae9afeb4
verss21 <arjunafrizal01@gmail.com>
2026-01-02 21:57:29 +0700

    menambahkan destinasi detail

---
+++ app/src/main/java/com/example/firebase/view/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.firebase.view.route
+
+import com.example.firebase.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
