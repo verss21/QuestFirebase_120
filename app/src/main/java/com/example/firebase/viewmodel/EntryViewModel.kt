166cccfe3c488c1412fc2975fa72af54e980d1f4
verss21 <arjunafrizal01@gmail.com>
2026-01-08 19:17:56 +0700

    menambahkan EntryViewModel

--- app/src/main/java/com/example/firebase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/EntryViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.firebase.viewmodel
 
+import RepositorySiswa
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -7,7 +8,6 @@ import androidx.lifecycle.ViewModel
 import com.example.firebase.modeldata.DetailSiswa
 import com.example.firebase.modeldata.UIStateSiswa
 import com.example.firebase.modeldata.toDataSiswa
-import com.example.firebase.repositori.RepositorySiswa
 
 class EntryViewModel(
     private val repositorySiswa: RepositorySiswa
@@ -16,31 +16,3 @@ class EntryViewModel(
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-    /* Fungsi untuk memvalidasi input */
-    private fun validasiInput(
-        uiState: DetailSiswa = uiStateSiswa.detailSiswa
-    ): Boolean {
-        return with(uiState) {
-            nama.isNotBlank() &&
-                    alamat.isNotBlank() &&
-                    telpon.isNotBlank()
-        }
-    }
-
-    fun updateUiState(detailSiswa: DetailSiswa) {
-        uiStateSiswa =
-            UIStateSiswa(
-                detailSiswa = detailSiswa,
-                isEntryValid = validasiInput(detailSiswa)
-            )
-    }
-
-    /* Fungsi untuk menyimpan data yang di-entry */
-    suspend fun addSiswa() {
-        if (validasiInput()) {
-            repositorySiswa.postDataSiswa(
-                uiStateSiswa.detailSiswa.toDataSiswa()
-            )
-        }
-    }
-}
