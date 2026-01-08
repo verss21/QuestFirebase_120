65e1cdc780d1100f2af9f42fa5fdbcdda8fb4814
verss21 <arjunafrizal01@gmail.com>
2026-01-08 19:15:57 +0700

    menambahkan class DetailViewModel

--- app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
@@ -18,3 +18,17 @@ sealed interface StatusUIDetail {
     object Loading : StatusUIDetail
 
 }
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa
+): ViewModel() {
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    var statusUIDetail:StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
