33314f74344cd006299f13ab90de537a14a79a51
verss21 <arjunafrizal01@gmail.com>
2026-01-08 19:13:09 +0700

    menambahkan composable fun BarisDetailData

--- app/src/main/java/com/example/firebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/firebase/view/HalamanDetail.kt
@@ -236,3 +236,16 @@ fun DetailDataSiswa(
         }
     }
 }
+
+@Composable
+private fun BarisDetailData(
+    @StringRes labelResID: Int,
+    itemDetail: String,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
+}
