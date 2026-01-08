820dd512ab9c7af892e46b091ca983edc3032bc5
verss21 <arjunafrizal01@gmail.com>
2026-01-08 18:49:48 +0700

    menambahkan fun hapusSatuSiswa

--- app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
@@ -71,3 +71,9 @@ class FirebaseRepositorySiswa: RepositorySiswa{
         ).await()
     }
 
+    override suspend fun hapusSatuSiswa(id: Long) {
+        val docQuery = collection.whereEqualTo("id", id).get().await()
+        val docId = docQuery.documents.firstOrNull()?.id ?: return
+        collection.document(docId).delete().await()
+    }
+}
