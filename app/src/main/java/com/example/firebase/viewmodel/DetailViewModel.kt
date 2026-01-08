662f7f1cbcc9528a73c20ed95582617f5f0bd66c
verss21 <arjunafrizal01@gmail.com>
2026-01-08 19:15:30 +0700

    menambahkan StatusUIDetail untuk detail siswa

---
+++ app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.firebase.viewmodel
+
+import RepositorySiswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebase.modeldata.Siswa
+import com.example.firebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+
+}
