2850120a0f7438de832f15524947fbbe817597c7
verss21 <arjunafrizal01@gmail.com>
2026-01-08 18:49:36 +0700

    menambahkan fun editSatuSiswa

--- app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
@@ -57,3 +57,17 @@ class FirebaseRepositorySiswa: RepositorySiswa{
             null
         }
     }
+    override suspend fun editSatuSiswa(id: Long, siswa: Siswa) {
+        val docQuery = collection.whereEqualTo("id", id).get().await()
+        val docId = docQuery.documents.firstOrNull()?.id ?: return
+
+        collection.document(docId).set(
+            mapOf(
+                "id" to siswa.id,
+                "nama" to siswa.nama,
+                "alamat" to siswa.alamat,
+                "telpon" to siswa.telpon
+            )
+        ).await()
+    }
+
